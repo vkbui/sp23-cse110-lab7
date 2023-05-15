e674eb5fe5180afa89e0e6aa7f4603dc6172ca40
vkbui <vuongkbui@gmail.com>
2023-05-14 22:47:03 -0700

    update main

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,8 +52,8 @@ function initializeServiceWorker() {
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
       try {
-        const registration = await navigator.serviceWorker.register("./sw.js", {
-          scope: "/",
+        const registration = await navigator.serviceWorker.register("sw.js", {
+          scope: ".",
         });
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
